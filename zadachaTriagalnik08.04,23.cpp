6615b2dff2e82e9175e02eeced40b9864e6f8ca5
AsenSirakov <asko2040@abv.bg>
2024-03-17 22:54:29 +0200

    Solution

---
+++ zadachaTriagalnik08.04,23.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+class Figura {
+public:
+    virtual double lice() = 0;
+};
+
+class Triagalnik : public Figura {
+public:
+    Triagalnik(double a, double b) : strana1(a), strana2(b) {}
+    double lice() {
+        return 0.5 * strana1 * strana2;
+    }
+private:
+    double strana1;
+    double strana2;
+};
+
+class Pravougulnik : public Figura {
+public:
+    Pravougulnik(double w, double h) : Strana1(w),Strana2(h) {}
+    double lice() {
+        return Strana1 * Strana2;
+    }
+  
+    double Strana1;
+    double Strana2;
+};
+
+int main() {
+	SetConsoleOutputCP(1251);
+    Triagalnik tria(3, 4);
+    cout << "Лице на правоъгълен триъгълник: " << tria.lice() << endl;
+    Pravougulnik sqr(3, 4);
+    cout << "Лице на правоъгълник: " << sqr.lice() << endl;
+    
+    return 0;
+}
