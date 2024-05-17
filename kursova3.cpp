a5bf4f448e93bf404f1d1591d6a8999cd01429a6
AsenSirakov <asko2040@abv.bg>
2024-05-17 20:05:43 +0300

    Solution

--- kursova3.cpp
+++ kursova3.cpp
@@ -9,6 +9,7 @@ private:
     int roomType;
     bool occupied;
     double price;
+    static int occupiedRoomsCount; 
 
 public:
     Room() {}
@@ -26,6 +27,10 @@ public:
             cout << "Invalid input. Please enter 0 for vacant or 1 for occupied: ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    
+        if (occupied) {
+            occupiedRoomsCount++;
         }
         cout << "Enter room price for Room " << roomNumber << ": $";
         cin >> price;
@@ -62,10 +67,16 @@ public:
     void writeToFile(ofstream& file) const {
         file << roomNumber << " " << roomType << " " << occupied << " " << price << endl;
     }
+
+    static int getOccupiedRoomsCount() {
+        return occupiedRoomsCount;
+    }
 };
 
+int Room::occupiedRoomsCount = 0;
+
 int main() {
-    Room hotelRooms[10]; // Assuming there are at most 10 rooms
+    Room hotelRooms[10]; 
     int choice;
     int roomCounter = 0;
 
@@ -74,10 +85,11 @@ int main() {
         cout << "2. Display room data" << endl;
         cout << "3. Check room occupancy" << endl;
         cout << "4. Write room data to file" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Check number of occupied rooms" << endl;
+        cout << "6. Exit" << endl;
         cout << "Enter your choice: ";
-        while (!(cin >> choice) || choice < 1 || choice > 5) {
-            cout << "Invalid input. Please enter a number between 1 and 5: ";
+        while (!(cin >> choice) || choice < 1 || choice > 6) {
+            cout << "Invalid input. Please enter a number between 1 and 6: ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
@@ -109,7 +121,7 @@ int main() {
                     cin >> roomNumberToCheck;
                 }
 
-                // Check if the entered room number is within the range of rooms for which data has been inputted
+                
                 if (roomNumberToCheck < 1 || roomNumberToCheck > roomCounter) {
                     cout << "No data input for Room " << roomNumberToCheck << ". Please try again." << endl;
                     break;
@@ -142,12 +154,15 @@ int main() {
                 break;
             }
             case 5:
+                cout << "Number of occupied rooms: " << Room::getOccupiedRoomsCount() << endl;
+                break;
+            case 6:
                 cout << "Exiting program." << endl;
                 break;
             default:
-                cout << "Invalid choice. Please enter a number between 1 and 5." << endl;
+                cout << "Invalid choice. Please enter a number between 1 and 6." << endl;
         }
-    } while (choice != 5);
+    } while (choice != 6);
 
     return 0;
 }
